e6b9a987c07a29cfadf708efa6ead8c095a97108
Kasia Kwiat <kasikkk222@vp.pl>
2016-10-12 11:10:06 +0200

    Dalsza stylizacja sekcji

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 $(document).foundation();
+$(document).ready(function() {
+    //var first_container =document.getElementById("contact-section-inner-2");
+	var container= document.getElementById("container-2");
+	document.getElementById("container-2").style.filter = "blur(8px)";
+	//document.getElementById("container-2").style.height = "0px";
+	for(var i=0;)
+  if(container.style.height== "0px"){
+  	document.getElementById("container-2").style.filter = "blur(8px)";
+  }
+
+  else{
+  	document.getElementById("container-2").style.filter = "blur(0px)";
+  }
+    
+        
+    });
